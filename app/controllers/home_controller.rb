a2fad7cb264057494b76b1779dc578398b174dd1
antoinelyset <antoinelyset+github@gmail.com>
2016-03-05 18:22:29 +0100

    Fix not found in HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.all { not_found }
+      format.all { head :not_found }
     end
   end
 end
