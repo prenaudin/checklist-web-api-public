38d5ace772e782f98fd1d9b9f0cef700f811579a
antoinelyset <antoinelyset+github@gmail.com>
2016-03-19 16:55:18 +0100

    Remove app/assets and app/frontend folders

--- app/frontend/javascripts/api/api.js
+++
@@ -1,5 +0,0 @@
-import local from './local'
-import server from './server'
-
-const api = server
-export default api
--- app/frontend/javascripts/store/store.js
+++
@@ -1,7 +0,0 @@
-import configureStore from './configureStore'
-
-const store = configureStore()
-
-window.store = store
-
-export default store
