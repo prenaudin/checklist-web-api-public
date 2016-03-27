25863bc4d44cfd37ee7dbc6eb2950e233f4cf2b5
antoinelyset <antoinelyset+github@gmail.com>
2016-03-27 22:18:46 +0200

    Refactor and spec UserShowChecklistContext

--- app/repositories/checklist_repository.rb
+++ app/repositories/checklist_repository.rb
@@ -4,4 +4,13 @@ class ChecklistRepository
   def self.model_class
     Checklist
   end
+
+  def self.find_with_user_and_project(user:, project:, checklist_id:)
+    all_with_user_and_project(user: user, project: project).find(checklist_id)
+  end
+
+  def self.all_with_user_and_project(user:, project:)
+    ProjectRepository.find_with_user(user: user,
+                                     project_id: project.id).checklists
+  end
 end
