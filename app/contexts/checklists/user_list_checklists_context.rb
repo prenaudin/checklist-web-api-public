54129f703421b4c6d8f9e27539d8831a3e58b2d8
antoinelyset <antoinelyset+github@gmail.com>
2016-03-27 22:36:00 +0200

    Refactor and Spec UserListChecklistsContext

--- app/contexts/checklists/user_list_checklists_context.rb
+++ app/contexts/checklists/user_list_checklists_context.rb
@@ -1,14 +1,14 @@
 class UserListChecklistsContext < ApplicationContext
-  attr_reader :user, :project
+  attr_reader :checklists
 
-  def initialize(user_id, project_id)
-    @user = User.find(user_id)
-    @project = Project.find(project_id)
-    #TODO Check Authorization
+  def initialize(user_id:, project_id:)
+    user = User.find(user_id)
+    project = ProjectRepository.find_with_user(user: user,
+                                               project_id: project_id)
+    @checklists = ChecklistRepository.all_with_project(project: project)
   end
 
   def call
-    checklists = project.checklists
     checklists
   end
 end
