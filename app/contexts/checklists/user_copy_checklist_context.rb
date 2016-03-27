996a0bc1cce95eb408f234f18978cf4a9e779cb6
antoinelyset <antoinelyset+github@gmail.com>
2016-03-27 23:40:12 +0200

    Refactor and Spec UserCopyChecklistContext

--- app/contexts/checklists/user_copy_checklist_context.rb
+++ app/contexts/checklists/user_copy_checklist_context.rb
@@ -1,19 +1,23 @@
 class UserCopyChecklistContext < ApplicationContext
-  attr_reader :user, :project, :checklist, :destination_project
+  attr_reader :checklist, :destination_project
 
-  def initialize(user_id, project_id, checklist_id, destination_project_id)
-    @user = User.find(user_id)
-    @destination_project = Project.find(destination_project_id)
-    #TODO Check Authorization
-    @project = Project.find(project_id)
-    @checklist = Checklist.find(checklist_id)
+  def initialize(user_id:, project_id:, checklist_id:, destination_project_id:)
+    user = UserRepository.find(user_id)
+    project = ProjectRepository.find_with_user(user: user,
+                                               project_id: project_id)
+    @checklist = ChecklistRepository
+                 .find_with_project(project: project,
+                                    checklist_id: checklist_id)
+    @destination_project = ProjectRepository
+                           .find_with_user(user: user,
+                                           project_id: destination_project_id)
   end
 
   def call
-    new_checklist = Checklist.new
-    new_checklist.title = checklist.title
+    new_checklist            = Checklist.new
+    new_checklist.title      = checklist.title
     new_checklist.test_suite = checklist.test_suite
-    new_checklist.project = destination_project
+    new_checklist.project    = destination_project
     new_checklist.save!
     new_checklist
   end
