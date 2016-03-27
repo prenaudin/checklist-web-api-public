052e852757b59996b317f05e6bf24597831cf52e
antoinelyset <antoinelyset+github@gmail.com>
2016-03-27 22:12:38 +0200

    Update Project's context specs styling

--- spec/contexts/projects/user_delete_project_context_spec.rb
+++ spec/contexts/projects/user_delete_project_context_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe UserDeleteProjectContext do
   let!(:project) { Factories.create_project(user: user) }
 
   it 'raises an error if the project is not managed by the user' do
-    expect { described_class.call(user_id: user_bis.id, project_id: project.id) }
-      .to raise_error(ActiveRecord::RecordNotFound)
+    expect do
+      described_class.call(user_id: user_bis.id, project_id: project.id)
+    end.to raise_error(ActiveRecord::RecordNotFound)
   end
 
   it 'deletes the project' do
