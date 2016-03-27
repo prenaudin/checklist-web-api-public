79006edb6d7b20feb1905eea317872e7b56f6035
antoinelyset <antoinelyset+github@gmail.com>
2016-03-27 22:18:24 +0200

    Add a security spec to UserCreateChecklistContext

--- spec/contexts/checklists/user_create_checklist_context_spec.rb
+++ spec/contexts/checklists/user_create_checklist_context_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe UserCreateChecklistContext do
-  let(:user) { Factories.create_user }
-  let(:project) { Factories.create_project(user: user) }
+  let(:user)        { Factories.create_user }
+  let(:user_bis)    { Factories.create_user }
+  let(:project)     { Factories.create_project(user: user) }
+  let(:project_bis) { Factories.create_project(user: user) }
   let(:test_suite) { ['Task 1', 'Task 2', 'Task 3'] }
   let(:params) do
     {
@@ -30,5 +32,13 @@ RSpec.describe UserCreateChecklistContext do
                                      project_id: project.id,
                                      params: params)
     expect(checklist.test_suite).to eq(test_suite)
+  end
+
+  it 'raises an error if the project is not managed by the user' do
+    expect do
+      described_class.call(user_id: user_bis.id,
+                           project_id: project.id,
+                           params: params)
+    end.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
